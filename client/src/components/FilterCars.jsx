4c51578180ba4a5424238afdfe72340f4064335d
unknown <ramovic225@gmail.com>
2024-07-22 18:33:42 +0200

    car filters added

--- client/src/components/FilterCars.jsx
+++ client/src/components/FilterCars.jsx
@@ -1,34 +1,141 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import ReactSlider$1 from "react-slider";
+import Select from "./Select.jsx";
+import Button from "./Button.jsx";
 
 const FilterCars = ({data, setFilteredCars}) => {
 
     const prices = data.map(car => car.price).filter(price => price !== null);
+    const years = data.map(car => car.year).filter(year => year !== null);
+    const driveOptions = [...new Set(data.map(car => car.drive))];
+    const fuelOptions = [...new Set(data.map(car => car.fuelType))];
+    const modelOptions = [...new Set(data.map(car => car.model))];
+    const transmissionOptions = [...new Set(data.map(car => car.transmission))];
+    const typeOptions = [...new Set(data.map(car => car.type))];
 
     const lowestPrice = Math.min(...prices);
     const highestPrice = Math.max(...prices);
+    const lowestYear = Math.min(...years);
+    const highestYear = Math.max(...years);
 
-    const [value, setValue] = useState([lowestPrice, highestPrice]);
+    const [priceValue, setPriceValue] = useState([lowestPrice, highestPrice]);
+    const [yearValue, setYearValue] = useState([lowestYear, highestYear]);
+    const [fuelType, setFuelType] = useState('All');
+    const [drive, setDrive] = useState('All');
+    const [model, setModel] = useState('All');
+    const [transmission, setTransmission] = useState('All');
+    const [type, setType] = useState('All');
+    console.log(data)
+
 
     const handleSubmit = () => {
-        const filtered = data.filter(car => car.price >= value[0] && car.price <= value[1]);
+        const filtered = data.filter(car =>
+            car.price >= priceValue[0] &&
+            car.price <= yearValue[1] &&
+            car.year >= yearValue[0] &&
+            car.year <= yearValue[1] &&
+            (model === 'All' || car.model === model) &&
+            (fuelType === 'All' || car.fuelType === fuelType) &&
+            (drive === 'All' || car.drive === drive)
+        )
+        console.log('clicked')
         setFilteredCars(filtered);
     }
 
+    const handleReset = () => {
+        setPriceValue([lowestPrice, highestPrice]);
+        setYearValue([lowestYear, highestYear]);
+        setFuelType('All');
+        setDrive('All');
+        setModel('All');
+        setTransmission('All');
+        setType('All');
+        setFilteredCars(data);
+    }
     return (
-        <div className="flex justify-center items-center flex-col">
-            <ReactSlider$1
-                className={"h-1 w-1/2 bg-red-100 flex items-center justify-center"}
-                min={lowestPrice}
-                max={highestPrice}
-                value={value}
-                onChange={setValue}
-                thumbClassName={"bg-red-500 w-4 h-4 rounded-full cursor-pointer"}
-            />
-            <p>Lowest Price: {lowestPrice}</p>
-            <p>Highest Price: {highestPrice}</p>
-            <p>Selected Price Range: {value[0]} - {value[1]}</p>
-            <button onClick={handleSubmit} className="bg-red-500 text-white font-semibold rounded-full px-4 py-2">Submit</button>
+        <div className="flex justify-center items-center flex-col w-full">
+            <div className="flex lg:flex-row flex-col justify-around gap-10 py-10">
+                <ReactSlider$1
+                    className="h-[25px] w-[400px] bg-gray-200 rounded-full flex items-center justify-center cursor-pointer"
+                    min={lowestPrice}
+                    max={highestPrice}
+                    value={priceValue}
+                    onChange={setPriceValue}
+                    renderThumb={(props, state) => (
+                        <div
+                            {...props}
+                            className="h-[25px] w-[25px] bg-black text-white rounded-full flex items-center justify-center cursor-grab"
+                        >
+                            <p className="text-xs">{state.valueNow}</p>
+                        </div>
+                    )}
+                    renderTrack={(props, state) => (
+                        <div
+                            {...props}
+                            className={`top-0 bottom-0 rounded-full ${state.index === 1 ? 'bg-red-500' : 'bg-gray-300'}`}
+                        />
+                    )}
+                />
+                <ReactSlider$1
+                    className="h-[25px] w-[400px] bg-gray-200 rounded-full flex items-center justify-center cursor-pointer"
+                    min={lowestYear}
+                    max={highestYear}
+                    value={yearValue}
+                    onChange={setYearValue}
+                    renderThumb={(props, state) => (
+                        <div
+                            {...props}
+                            className="h-[25px] w-[25px] bg-black text-white rounded-full flex items-center justify-center cursor-grab"
+                        >
+                            <p className="text-xs">{state.valueNow}</p>
+                        </div>
+                    )}
+                    renderTrack={(props, state) => (
+                        <div
+                            {...props}
+                            className={`top-0 bottom-0 rounded-full ${state.index === 1 ? 'bg-red-500' : 'bg-gray-300'}`}
+                        />
+                    )}
+                />
+            </div>
+            <div className="w-full bg-red-800 flex items-center justify-center py-4">
+                <div className="w-[95%] mx-auto grid sm:grid-cols-2 xl:grid-cols-6 gap-2 items-end">
+                    <Select setSelected={setModel}
+                            options={['All', ...modelOptions]}
+                            label="Model"
+                            selected={model}
+                            className={"min-w-[180px] cursor-pointer"}
+                    />
+                    <Select setSelected={setFuelType}
+                            options={['All', ...fuelOptions]}
+                            label="Fuel Type"
+                            selected={fuelType}
+                            className={"min-w-[180px] cursor-pointer"}
+                    />
+                    <Select setSelected={setDrive}
+                            label="Drive"
+                            options={['All', ...driveOptions]}
+                            selected={drive}
+                            className={"min-w-[180px] cursor-pointer"}
+                    />
+                    <Select setSelected={setTransmission}
+                            label="Transmission"
+                            options={['All', ...transmissionOptions]}
+                            selected={transmission}
+                            className={"min-w-[180px] cursor-pointer"}
+                    />
+                    <Select setSelected={setType}
+                            label="Type"
+                            options={['All', ...typeOptions]}
+                            selected={type}
+                            className={"min-w-[180px] cursor-pointer"}
+                    />
+                    <div className="flex gap-4 w-full h-fit">
+                        <Button onClick={handleReset} label="Reset" className="flex-grow bg-gray-900 text-white font-semibold rounded-sm py-3"/>
+                        <Button onClick={handleSubmit} label="Search" className="flex-grow bg-gray-900 text-white font-semibold rounded-sm py-3"/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,11 +25,13 @@ const Home = () => {
             <div className="relative">
                 <img src={car} alt="car" className="w-full h-auto max-h-[600px] object-cover object-center"/>
                 <div className="absolute -translate-y-[30%] left-1/2 -translate-x-1/2 w-[130%] md:w-[110%] h-[250px] bg-white rounded-t-[50%]">
-                    <FilterCars data={cars} setFilteredCars={setFilteredCars}/>
                     <div className="py-10 flex flex-col justify-center items-center">
                         <h3 className="text-xl font-semibold text-center uppercase leading-5">Check out</h3>
                         <h1 className="text-4xl text-red-500 font-semibold text-center leading-5">Our Cars</h1>
                     </div>
+                </div>
+                <div className={"relative flex justify-center items-center rounded-t-full h-[300px]"}>
+                    <FilterCars data={cars} setFilteredCars={setFilteredCars}/>
                 </div>
             </div>
             <div className="relative flex justify-center py-10">
